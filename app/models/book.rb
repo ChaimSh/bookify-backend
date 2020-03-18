1e86c966989ab8b5618fe96665a358f1d2fb8de6
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-03-18 16:09:17 -0500

    model relationships

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+
+  belongs_to :authors
+  belongs_to :users
+  has_many :genres
+  has_many :comments
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
+
+    has_many :books
+    has_many :authors, through: :books
+    has_many :genres, through: :books
+    has_many :comments
+    # has_many :comments, through: :books
+    validates :username, uniqueness: true
 end
