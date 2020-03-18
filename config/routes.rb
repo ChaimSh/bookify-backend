3fcd8bf24deadc3d98d342918e4af7edb483b23f
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-03-18 15:39:32 -0500

    scaffold comments genres and authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  
+
   namespace :api do
     namespace :v1 do   
+      resources :comments
+      resources :genres
+      resources :authors
       resources :users
       resources :books
     end
