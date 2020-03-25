af0d501741183cde37d48c48ffe82003ff557b25
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-03-24 21:17:22 -0500

    fix routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# user = User.create!(name: "chaim", username: "chaims", password: "123")
+
+# Author.create(name: "Dovid")
+
+# tehillim = Book.create!(title:"tehillim", author_id: Author.first)
+
+# user.books.create!(title: "tehillim")
+
+User.create(name: "Dovid", username: "dovidg", password: "123")
