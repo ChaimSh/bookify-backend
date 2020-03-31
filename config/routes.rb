e4a7c4a80008f378fc5138f1a73b316ccb5f1019
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-03-30 22:09:38 -0500

    fix book serialization

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   post "/api/v1/login", to: "sessions#create" 
   delete "/api/v1/logout", to: "sessions#destroy" 
   get "/api/v1/get_current_user", to: "sessions#get_current_user"
+  
+  resources :users 
+  
   namespace :api do
     namespace :v1 do   
       resources :comments
       resources :genres
       resources :authors
       resources :books
-      resources :users do
-        resources :books, only: [:index]
-      end
     end
   end
 end
